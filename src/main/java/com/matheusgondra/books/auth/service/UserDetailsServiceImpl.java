d0e1f896f9136c18e7307035fe4bd57e1bdb7335
matheusgondra <matheusgondra951@gmail.com>
2026-02-11 12:55:33 -0300

    feat: create UserDetailsServiceImpl

---
+++ src/main/java/com/matheusgondra/books/auth/service/UserDetailsServiceImpl.java
@@ -0,0 +1,22 @@
+package com.matheusgondra.books.auth.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.matheusgondra.books.user.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + username));
+    }
+}
