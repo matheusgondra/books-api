3a75c14acf5bb5f935bd7077e8837191ffe62cd2
matheusgondra <matheusgondra951@gmail.com>
2026-02-11 15:04:04 -0300

    feat: create beans

--- src/main/java/com/matheusgondra/books/auth/service/UserDetailsServiceImpl.java
+++ src/main/java/com/matheusgondra/books/auth/service/UserDetailsServiceImpl.java
@@ -3,13 +3,11 @@ package com.matheusgondra.books.auth.service;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
 
 import com.matheusgondra.books.user.repository.UserRepository;
 
 import lombok.RequiredArgsConstructor;
 
-@Service
 @RequiredArgsConstructor
 public class UserDetailsServiceImpl implements UserDetailsService {
     private final UserRepository userRepository;
--- src/main/java/com/matheusgondra/books/config/SecurityConfig.java
+++ src/main/java/com/matheusgondra/books/config/SecurityConfig.java
@@ -2,12 +2,18 @@ package com.matheusgondra.books.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import com.matheusgondra.books.auth.service.UserDetailsServiceImpl;
+import com.matheusgondra.books.user.repository.UserRepository;
 
 @Configuration
 @EnableWebSecurity
@@ -35,5 +41,15 @@ public class SecurityConfig {
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    UserDetailsService userDetailsService(UserRepository userRepository) {
+        return new UserDetailsServiceImpl(userRepository);
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
 }
