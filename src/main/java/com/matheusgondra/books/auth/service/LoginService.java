22a0b2cf5f7285d4434aab8b789fa89a5823f11e
matheusgondra <matheusgondra951@gmail.com>
2026-02-11 22:15:09 -0300

    feat: ensure LoginService call TokenService.generateToken with correct value

--- src/main/java/com/matheusgondra/books/auth/service/LoginService.java
+++ src/main/java/com/matheusgondra/books/auth/service/LoginService.java
@@ -2,11 +2,13 @@ package com.matheusgondra.books.auth.service;
 
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 import com.matheusgondra.books.auth.usecase.login.Login;
 import com.matheusgondra.books.auth.usecase.login.LoginData;
 import com.matheusgondra.books.auth.usecase.login.LoginResponse;
+import com.matheusgondra.books.user.model.User;
 
 import lombok.RequiredArgsConstructor;
 
@@ -14,12 +16,17 @@ import lombok.RequiredArgsConstructor;
 @Service
 public class LoginService implements Login {
     private final AuthenticationManager authenticationManager;
+    private final TokenService tokenService;
 
     @Override
     public LoginResponse execute(LoginData data) {
         UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(data.email(),
                 data.password());
-        authenticationManager.authenticate(authenticationToken);
+        Authentication authentication = authenticationManager.authenticate(authenticationToken);
+
+        User user = (User) authentication.getPrincipal();
+        tokenService.generateToken(user.getId().toString());
+
         return null;
     }
 }
