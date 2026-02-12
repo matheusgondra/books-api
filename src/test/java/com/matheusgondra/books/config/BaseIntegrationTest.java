d75c9312fd7711f59b356c41bc393c987eabbe5c
matheusgondra <matheusgondra951@gmail.com>
2026-02-11 23:13:32 -0300

    fix: fix testcontainers

--- src/test/java/com/matheusgondra/books/config/BaseIntegrationTest.java
+++ src/test/java/com/matheusgondra/books/config/BaseIntegrationTest.java
@@ -3,26 +3,35 @@ package com.matheusgondra.books.config;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 
 import io.restassured.RestAssured;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@Testcontainers
 public abstract class BaseIntegrationTest {
-    @Container
-    @ServiceConnection
-    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(DockerImageName.parse("postgres:15-alpine"));
+    static final PostgreSQLContainer<?> postgres;
 
     @LocalServerPort
     private int port;
+
+    static {
+        postgres = new PostgreSQLContainer<>(DockerImageName.parse("postgres:15-alpine"));
+        postgres.start();
+    }
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
 
     @BeforeEach
     void setUpRestAssured() {
         RestAssured.port = port;
     }
+
 }
