9f04e58c97b57c0f073423415506844b5d7db6d7
matheusgondra <matheusgondra951@gmail.com>
2026-02-13 12:34:39 -0300

    fix: fix DI for TokenService

--- src/main/java/com/matheusgondra/books/auth/service/TokenService.java
+++ src/main/java/com/matheusgondra/books/auth/service/TokenService.java
@@ -3,18 +3,20 @@ package com.matheusgondra.books.auth.service;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
 
-import lombok.RequiredArgsConstructor;
-
 @Service
-@RequiredArgsConstructor
 public class TokenService {
     private final String secretKey;
+
+    public TokenService(@Value("${api.jwt.secret}") String secretKey) {
+        this.secretKey = secretKey;
+    }
 
     public String generateToken(String payload) {
         Algorithm algorithm = Algorithm.HMAC256(secretKey);
