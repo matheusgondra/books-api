16fe73c9158aaca440890ad898ad5ea953feeff2
matheusgondra <matheusgondra951@gmail.com>
2026-04-06 08:53:34 -0300

    test:: ensure RegisterAutorService throw AuthorAlreadyExistsException if author already exists

--- src/test/java/com/matheusgondra/books/author/usecase/RegisterAuthorServiceTest.java
+++ src/test/java/com/matheusgondra/books/author/usecase/RegisterAuthorServiceTest.java
@@ -1,8 +1,12 @@
 package com.matheusgondra.books.author.usecase;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -13,6 +17,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import com.matheusgondra.books.author.model.Author;
 import com.matheusgondra.books.author.repository.AuthorRepository;
 import com.matheusgondra.books.author.usecase.register.author.RegisterAuthorData;
+import com.matheusgondra.books.exception.AuthorAlreadyExistsException;
 
 @ExtendWith(MockitoExtension.class)
 public class RegisterAuthorServiceTest {
@@ -24,10 +29,22 @@ public class RegisterAuthorServiceTest {
     @Mock
     private AuthorRepository repository;
 
+    @Mock
+    private Author authorMock;
+
     @Test
     void shouldCallSaveMethodOnRepository() {
         sut.execute(dataMock);
 
         verify(repository, times(1)).save(any(Author.class));
+    }
+
+    @Test
+    void shouldThrowAuthorAlreadyExistsIfAuthorAlreadyExists() {
+        Author authorMock = new Author(dataMock.name());
+
+        when(repository.findByName(dataMock.name())).thenReturn(Optional.of(authorMock));
+
+        assertThrows(AuthorAlreadyExistsException.class, () -> sut.execute(dataMock));
     }
 }
