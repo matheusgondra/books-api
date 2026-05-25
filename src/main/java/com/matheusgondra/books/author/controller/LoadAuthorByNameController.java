5f8767736730873229ce6b3d92ac5ac0e7dfbc53
matheusgondra <matheusgondra951@gmail.com>
2026-05-24 22:39:05 -0300

    feat: ensure GET /api/author/loadByName/{name} return 200 on success

---
+++ src/main/java/com/matheusgondra/books/author/controller/LoadAuthorByNameController.java
@@ -0,0 +1,30 @@
+package com.matheusgondra.books.author.controller;
+
+import com.matheusgondra.books.author.model.Author;
+import com.matheusgondra.books.author.usecase.load.author.LoadAuthorByName;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/author/loadByName")
+public class LoadAuthorByNameController {
+    private final LoadAuthorByName useCase;
+
+    @GetMapping("{name}")
+    public ResponseEntity<Author> handle(@PathVariable String name) {
+        log.debug("Receive name: {}", name);
+
+        Author result = this.useCase.execute(name);
+
+        log.debug("Use case result: {}", result);
+
+        return ResponseEntity.ok(result);
+    }
+}
