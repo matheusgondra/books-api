cbf28afd6fa95bafa94c30fc535299cbf69b1b42
matheusgondra <matheusgondra951@gmail.com>
2026-06-08 22:14:00 -0300

    feat: ensure route GET /api/author return 200 on success

---
+++ src/main/java/com/matheusgondra/books/author/controller/LoadAuthorsController.java
@@ -0,0 +1,34 @@
+package com.matheusgondra.books.author.controller;
+
+import com.matheusgondra.books.author.model.Author;
+import com.matheusgondra.books.author.usecase.load.author.LoadAuthors;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/author")
+public class LoadAuthorsController {
+    private final LoadAuthors useCase;
+
+    @GetMapping
+    public ResponseEntity<Page<Author>> handle(
+            @RequestParam(required = false, defaultValue = "0") Integer page,
+            @RequestParam(required = false, defaultValue = "10") Integer size) {
+        log.debug("Receive page {} and size {}", page, size);
+
+        Pageable pageable = PageRequest.of(page, size);
+        Page<Author> result = this.useCase.execute(pageable);
+
+        return ResponseEntity.ok(result);
+    }
+}
