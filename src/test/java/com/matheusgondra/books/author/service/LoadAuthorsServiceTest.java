535c030dcb88a379af89ffc7d3bd7d1b3cd98b79
matheusgondra <matheusgondra951@gmail.com>
2026-06-08 21:40:18 -0300

    test: ensure LoadAuthorsService return a page on success

--- src/test/java/com/matheusgondra/books/author/service/LoadAuthorsServiceTest.java
+++ src/test/java/com/matheusgondra/books/author/service/LoadAuthorsServiceTest.java
@@ -1,14 +1,20 @@
 package com.matheusgondra.books.author.service;
 
+import com.matheusgondra.books.author.model.Author;
 import com.matheusgondra.books.author.repository.AuthorRepository;
+import com.matheusgondra.books.factory.AuthorFactory;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.ActiveProfiles;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -22,7 +28,14 @@ public class LoadAuthorsServiceTest {
     @Mock
     private AuthorRepository repository;
 
-    private final Pageable pageable = Pageable.unpaged();
+    private final Pageable pageable = PageRequest.of(0, 10);
+
+    @BeforeEach
+    void setUp() {
+        Page<Author> page = AuthorFactory.createPage();
+
+        when(repository.findAll(pageable)).thenReturn(page);
+    }
 
     @Test
     void shouldCallFindAllOnRepository() {
@@ -36,5 +49,13 @@ public class LoadAuthorsServiceTest {
         when(repository.findAll(pageable)).thenThrow(new RuntimeException());
 
         assertThrows(RuntimeException.class, () -> sut.execute(pageable));
+    }
+
+    @Test
+    void shouldReturnPageOnSuccess() {
+        Page<Author> result = sut.execute(pageable);
+
+        assertEquals(3, result.getTotalElements());
+        assertEquals(1, result.getTotalPages());
     }
 }
--- src/test/java/com/matheusgondra/books/factory/AuthorFactory.java
+++ src/test/java/com/matheusgondra/books/factory/AuthorFactory.java
@@ -1,12 +1,23 @@
 package com.matheusgondra.books.factory;
 
-import java.time.LocalDateTime;
-import java.util.UUID;
-
 import com.matheusgondra.books.author.model.Author;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
 
 public class AuthorFactory {
     public static Author create() {
-        return new Author(UUID.randomUUID(), "anyName", LocalDateTime.now(), LocalDateTime.now());
+        LocalDateTime now = LocalDateTime.now();
+
+        return new Author(UUID.randomUUID(), "anyName", now, now);
+    }
+
+    public static Page<Author> createPage() {
+        List<Author> authors = List.of(create(), create(), create());
+
+        return new PageImpl<>(authors);
     }
 }
