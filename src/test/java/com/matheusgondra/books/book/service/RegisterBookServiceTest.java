3a12c3661b0379f8133c394ff1d12dba38ce15d0
matheusgondra <matheusgondra951@gmail.com>
2026-06-15 12:31:03 -0300

    test: ensure RegisterBookService throw AuthorNotFoundException when author not found

--- src/test/java/com/matheusgondra/books/book/service/RegisterBookServiceTest.java
+++ src/test/java/com/matheusgondra/books/book/service/RegisterBookServiceTest.java
@@ -1,13 +1,21 @@
 package com.matheusgondra.books.book.service;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.lenient;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import com.matheusgondra.books.author.exception.AuthorNotFoundException;
+import com.matheusgondra.books.author.model.Author;
 import com.matheusgondra.books.author.repository.AuthorRepository;
 import com.matheusgondra.books.book.usecase.register.RegisterBookData;
 
@@ -25,10 +33,22 @@ public class RegisterBookServiceTest {
     @Mock
     private AuthorRepository authorRepository;
 
+    @BeforeEach
+    void setUp() {
+        lenient().when(authorRepository.findByName(data.author())).thenReturn(Optional.of(new Author(data.author())));
+    }
+
     @Test
     void shouldCallFindByNameOnAuthorRepository() {
         sut.execute(data);
 
         verify(authorRepository).findByName(data.author());
+    }
+
+    @Test
+    void shouldAuthorNotFoundExceptionWhenAuthorNotFound() {
+        when(authorRepository.findByName(data.author())).thenReturn(Optional.empty());
+
+        assertThrows(AuthorNotFoundException.class, () -> sut.execute(data));
     }
 }
