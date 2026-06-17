c3093e4d6945ef0a3f0296f178168e3e300aea8a
matheusgondra <matheusgondra951@gmail.com>
2026-06-17 13:14:39 -0300

    test: ensure LoadBooksService return page of BookDetails on success

--- src/test/java/com/matheusgondra/books/book/service/LoadBooksServiceTest.java
+++ src/test/java/com/matheusgondra/books/book/service/LoadBooksServiceTest.java
@@ -1,16 +1,23 @@
 package com.matheusgondra.books.book.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
+import com.matheusgondra.books.book.dto.BookDetails;
+import com.matheusgondra.books.book.model.Book;
 import com.matheusgondra.books.book.repository.BookRepository;
+import com.matheusgondra.books.factory.BookFactory;
 
 @ExtendWith(MockitoExtension.class)
 public class LoadBooksServiceTest {
@@ -22,10 +29,26 @@ public class LoadBooksServiceTest {
     @Mock
     private BookRepository repository;
 
+    @BeforeEach
+    void setUp() {
+        Page<Book> page = BookFactory.createPage();
+
+        when(repository.findAll(pageable)).thenReturn(page);
+    }
+
     @Test
     void shouldCallFindAllOnRepository() {
         sut.execute(pageable);
 
         verify(repository).findAll(pageable);
+    }
+
+    @Test
+    void shouldReturnPageOfBookDetailsOnSuccess() {
+        Page<BookDetails> result = sut.execute(pageable);
+
+        var expected = BookFactory.createDetailsPage();
+
+        assertEquals(expected, result);
     }
 }
--- src/test/java/com/matheusgondra/books/factory/BookFactory.java
+++ src/test/java/com/matheusgondra/books/factory/BookFactory.java
@@ -1,6 +1,12 @@
 package com.matheusgondra.books.factory;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+
 import com.matheusgondra.books.author.model.Author;
+import com.matheusgondra.books.book.dto.BookDetails;
 import com.matheusgondra.books.book.model.Book;
 
 public class BookFactory {
@@ -12,6 +18,12 @@ public class BookFactory {
                 .build();
     }
 
+    public static Book create(boolean withAuthor) {
+        Author author = AuthorFactory.create();
+
+        return create(author);
+    }
+
     public static Book create(Author author) {
         return Book.builder()
                 .title("anyTitle")
@@ -19,5 +31,15 @@ public class BookFactory {
                 .pages(120)
                 .author(author)
                 .build();
+    }
+
+    public static Page<Book> createPage() {
+        List<Book> books = List.of(create(true), create(true), create(true));
+
+        return new PageImpl<>(books);
+    }
+
+    public static Page<BookDetails> createDetailsPage() {
+        return createPage().map(BookDetails::new);
     }
 }
