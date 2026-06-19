3be6e0c6b30b6deeef2bd06384ede266594fd051
matheusgondra <matheusgondra951@gmail.com>
2026-06-19 15:16:45 -0300

    test: ensure LoadBookByIdService throw BookNotFoundException when book does not exist

--- src/test/java/com/matheusgondra/books/book/service/LoadBookByIdServiceTest.java
+++ src/test/java/com/matheusgondra/books/book/service/LoadBookByIdServiceTest.java
@@ -1,9 +1,13 @@
 package com.matheusgondra.books.book.service;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import java.util.Optional;
 import java.util.UUID;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -11,6 +15,8 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.matheusgondra.books.book.repository.BookRepository;
+import com.matheusgondra.books.exception.BookNotFoundException;
+import com.matheusgondra.books.factory.BookFactory;
 
 @ExtendWith(MockitoExtension.class)
 public class LoadBookByIdServiceTest {
@@ -22,10 +28,22 @@ public class LoadBookByIdServiceTest {
     @Mock
     private BookRepository repository;
 
+    @BeforeEach
+    void setUp() {
+        when(repository.findById(id)).thenReturn(Optional.of(BookFactory.create()));
+    }
+
     @Test
     void shouldCallFindByIdOnRepository() {
         sut.execute(id);
 
         verify(repository).findById(id);
+    }
+
+    @Test
+    void shouldThrowBookNotFoundExceptionWhenBookDoesNotExist() {
+        when(repository.findById(id)).thenReturn(Optional.empty());
+
+        assertThrows(BookNotFoundException.class, () -> sut.execute(id));
     }
 }
