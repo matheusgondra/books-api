26b0ce07319fdafc1471410f38a9e2b112eec1c6
matheusgondra <matheusgondra951@gmail.com>
2026-06-22 07:35:26 -0300

    test: ensure DeleteBookService throw BookNotFoundException when book does not exist

--- src/test/java/com/matheusgondra/books/book/service/DeleteBookServiceTest.java
+++ src/test/java/com/matheusgondra/books/book/service/DeleteBookServiceTest.java
@@ -1,10 +1,15 @@
 package com.matheusgondra.books.book.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import com.matheusgondra.books.book.model.Book;
 import com.matheusgondra.books.book.repository.BookRepository;
+import com.matheusgondra.books.exception.BookNotFoundException;
+import java.util.Optional;
 import java.util.UUID;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -21,10 +26,22 @@ class DeleteBookServiceTest {
 
     private final UUID id = UUID.randomUUID();
 
+    @BeforeEach
+    void setUp() {
+        when(repository.findById(id)).thenReturn(Optional.of(new Book()));
+    }
+
     @Test
     void shouldCallDeleteByIdOnRepository() {
         sut.execute(id);
 
         verify(repository).deleteById(id);
+    }
+
+    @Test
+    void shouldThrowBookNotFoundExceptionWhenBookDoesNotExist() {
+        when(repository.findById(id)).thenReturn(Optional.empty());
+
+        assertThrows(BookNotFoundException.class, () -> sut.execute(id));
     }
 }
