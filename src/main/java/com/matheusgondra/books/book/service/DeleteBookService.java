f2661a5d1a7128c8eb10365d8aac867ba7863e18
matheusgondra <matheusgondra951@gmail.com>
2026-06-22 07:34:28 -0300

    feat: ensure DeleteBookService throw BookNotFoundException when book does not exist

--- src/main/java/com/matheusgondra/books/book/service/DeleteBookService.java
+++ src/main/java/com/matheusgondra/books/book/service/DeleteBookService.java
@@ -2,6 +2,7 @@ package com.matheusgondra.books.book.service;
 
 import com.matheusgondra.books.book.repository.BookRepository;
 import com.matheusgondra.books.book.usecase.delete.DeleteBook;
+import com.matheusgondra.books.exception.BookNotFoundException;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,6 +14,8 @@ public class DeleteBookService implements DeleteBook {
 
     @Override
     public void execute(UUID id) {
+        this.repository.findById(id).orElseThrow(BookNotFoundException::new);
+
         repository.deleteById(id);
     }
 }
