06f4c0d1624496b3532df7b68e08f05548fa9662
matheusgondra <matheusgondra951@gmail.com>
2026-06-21 21:14:26 -0300

    test: ensure DeleteBookService call deleteById on repository

--- src/test/java/com/matheusgondra/books/book/service/DeleteBookServiceTest.java
+++ src/test/java/com/matheusgondra/books/book/service/DeleteBookServiceTest.java
@@ -1,4 +1,30 @@
+package com.matheusgondra.books.book.service;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+
+import com.matheusgondra.books.book.repository.BookRepository;
+import java.util.UUID;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
 class DeleteBookServiceTest {
-  
+    @InjectMocks
+    private DeleteBookService sut;
+
+    @Mock
+    private BookRepository repository;
+
+    private final UUID id = UUID.randomUUID();
+
+    @Test
+    void shouldCallDeleteByIdOnRepository() {
+        sut.execute(id);
+
+        verify(repository).deleteById(id);
+    }
 }
