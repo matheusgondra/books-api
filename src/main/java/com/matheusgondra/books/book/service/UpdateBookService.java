c0e45a95f075561fbd87f0b2523f9539059a524e
matheusgondra <matheusgondra951@gmail.com>
2026-06-25 16:08:16 -0300

    feat: ensure UpdateBookServer throw AuthorNotFoundException when author is different and not found

--- src/main/java/com/matheusgondra/books/book/service/UpdateBookService.java
+++ src/main/java/com/matheusgondra/books/book/service/UpdateBookService.java
@@ -1,6 +1,9 @@
 package com.matheusgondra.books.book.service;
 
+import com.matheusgondra.books.author.exception.AuthorNotFoundException;
+import com.matheusgondra.books.author.repository.AuthorRepository;
 import com.matheusgondra.books.book.dto.BookDetails;
+import com.matheusgondra.books.book.model.Book;
 import com.matheusgondra.books.book.repository.BookRepository;
 import com.matheusgondra.books.book.usecase.update.UpdateBook;
 import com.matheusgondra.books.book.usecase.update.UpdateBookData;
@@ -14,11 +17,16 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class UpdateBookService implements UpdateBook {
     private final BookRepository bookRepository;
+    private final AuthorRepository authorRepository;
 
     @Override
     @Transactional
     public BookDetails execute(UUID id, UpdateBookData data) {
-        this.bookRepository.findWithAuthorById(id).orElseThrow(BookNotFoundException::new);
+        Book book = this.bookRepository.findWithAuthorById(id).orElseThrow(BookNotFoundException::new);
+
+        if (data.author() != null && !data.author().equals(book.getAuthor().getName())) {
+            this.authorRepository.findByName(data.author()).orElseThrow(AuthorNotFoundException::new);
+        }
 
         return null;
     }
