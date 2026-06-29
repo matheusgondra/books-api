1c596c19cdf014cce3430b4fec8393c46f482e71
matheusgondra <matheusgondra951@gmail.com>
2026-06-29 12:41:09 -0300

    feat: ensure route DELETE /api/book/{id} returns 204 on success

---
+++ src/main/java/com/matheusgondra/books/book/controller/DeleteBookController.java
@@ -0,0 +1,28 @@
+package com.matheusgondra.books.book.controller;
+
+import com.matheusgondra.books.book.usecase.delete.DeleteBook;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/book")
+public class DeleteBookController {
+    private final DeleteBook useCase;
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> deleteBook(@PathVariable UUID id) {
+        log.info("Deleting book with id: {}", id);
+
+        this.useCase.execute(id);
+
+        return ResponseEntity.noContent().build();
+    }
+}
