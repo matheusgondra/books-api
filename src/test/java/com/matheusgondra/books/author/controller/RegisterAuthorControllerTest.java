cbce588b824376366135e8c539ac6d9b41210fb1
matheusgondra <matheusgondra951@gmail.com>
2026-07-01 07:22:48 -0300

    test: ensure POST /api/author/register

--- src/test/java/com/matheusgondra/books/author/controller/RegisterAuthorControllerTest.java
+++ src/test/java/com/matheusgondra/books/author/controller/RegisterAuthorControllerTest.java
@@ -1,48 +1,68 @@
 package com.matheusgondra.books.author.controller;
 
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
+
 import com.matheusgondra.books.author.dto.request.RegisterAuthorRequestDTO;
 import com.matheusgondra.books.config.BaseIntegrationTest;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import tools.jackson.databind.ObjectMapper;
 
 public class RegisterAuthorControllerTest extends BaseIntegrationTest {
     private final RegisterAuthorRequestDTO dto = new RegisterAuthorRequestDTO("anyAuthor");
     private final String path = "/api/author/register";
 
-    @Autowired
-    private ObjectMapper objectMapper;
-
     @Test
     void shouldReturn201OnSuccess() {
         RestAssured.given()
                 .contentType(ContentType.JSON)
-                .body(objectMapper.writeValueAsString(dto))
+                .body(dto)
                 .header("Authorization", "Bearer " + accessToken)
                 .when()
                 .post(path)
                 .then()
-                .statusCode(201);
+                .contentType(ContentType.JSON)
+                .statusCode(201)
+                .body("id", notNullValue())
+                .body("name", equalTo(dto.name()))
+                .body("createdAt", notNullValue())
+                .body("updatedAt", notNullValue());
     }
 
     @Test
     void shouldReturn409IfAuthorAlreadyExists() {
         RestAssured.given()
                 .contentType(ContentType.JSON)
-                .body(objectMapper.writeValueAsString(dto))
+                .body(dto)
                 .header("Authorization", "Bearer " + accessToken)
                 .when()
                 .post(path);
 
         RestAssured.given()
                 .contentType(ContentType.JSON)
-                .body(objectMapper.writeValueAsString(dto))
+                .body(dto)
                 .header("Authorization", "Bearer " + accessToken)
                 .when()
                 .post(path)
                 .then()
-                .statusCode(409);
+                .contentType(ContentType.JSON)
+                .statusCode(409)
+                .body("message", equalTo("Author already exists"))
+                .body("status", equalTo(409));
+    }
+
+    @Test
+    void shouldReturn401WhenNoAuthenticated() {
+        RestAssured.given()
+                .contentType(ContentType.JSON)
+                .body(dto)
+                .when()
+                .post(path)
+                .then()
+                .contentType(ContentType.JSON)
+                .statusCode(401)
+                .body("message", equalTo("Unauthorized"))
+                .body("status", equalTo(401));
     }
 }
