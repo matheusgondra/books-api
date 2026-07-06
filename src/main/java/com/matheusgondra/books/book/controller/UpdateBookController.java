4e211f95c24008b0b1963a8febab72fa22f7e36c
matheusgondra <matheusgondra951@gmail.com>
2026-07-06 07:30:36 -0300

    feat: add xml support in PUT /api/book/{id}

--- src/main/java/com/matheusgondra/books/book/controller/UpdateBookController.java
+++ src/main/java/com/matheusgondra/books/book/controller/UpdateBookController.java
@@ -11,6 +11,7 @@ import jakarta.validation.Valid;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -24,7 +25,10 @@ public class UpdateBookController {
     private final UpdateBook useCase;
 
     @UpdateBookControllerDoc
-    @PutMapping("{id}")
+    @PutMapping(
+            value = "{id}",
+            consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
     public ResponseEntity<BookDetails> handle(@PathVariable UUID id, @RequestBody @Valid UpdateBookRequestDTO dto) {
         log.debug("Update book {} with data {}", id, dto);
 
